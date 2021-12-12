bfb8f97b0d596761c42885127c256614cace7323
samuzaffar99 <sam.muzaffar99@gmail.com>
2021-12-12 18:14:08 +0500

    added classification code

--- lib/tflite/classifier.dart
+++ lib/tflite/classifier.dart
@@ -19,14 +19,17 @@ class Classifier {
   /// Labels file loaded as list
   List<String>? _labels;
 
-  static const String modelFileName = "ssd_mobilenetv2.tflite";
-  static const String labelFileName = "ssd_mobilenetv2.txt";
+  // static const String modelFileName = "ssd_mobilenetv2.tflite";
+  // static const String labelFileName = "ssd_mobilenetv2.txt";
+
+  static const String modelFileName = "efficientnet/quantized_uint8.tflite";
+  static const String labelFileName = "efficientnet/Classes.txt";
 
   /// Input size of image (height = width = 300)
-  static const int inputSize = 320;
+  static const int inputSize = 224;
 
   /// Result score threshold
-  static const double threshold = 0.35;
+  static const double threshold = 0.8;
 
   /// [ImageProcessor] used to pre-process the image
   ImageProcessor? imageProcessor;
@@ -56,12 +59,10 @@ class Classifier {
     try {
       // print("try load interpreter");
       _interpreter = interpreter ??
-          await Interpreter.fromAsset(
-            modelFileName,
-            options: InterpreterOptions()
-              ..threads = 4
-              ..useFlexDelegateAndroid = true,
-          );
+          await Interpreter.fromAsset(modelFileName,
+              options: InterpreterOptions()..threads = 4
+              // ..useFlexDelegateAndroid = true,
+              );
       // print("loaded interpreter");
       var outputTensors = _interpreter!.getOutputTensors();
       _outputShapes = [];
@@ -75,25 +76,10 @@ class Classifier {
     }
   }
 
-  Future<void> loadClasses() async {
-    String data = await rootBundle.loadString('assets/classes.txt');
-    _filterClasses = LineSplitter.split(data).toList();
-    print(_filterClasses);
-    return;
-  }
-
-  List<String>? _filterClasses;
-
   /// Loads labels from assets
   void loadLabels({List<String>? labels}) async {
     try {
       _labels = labels ?? await FileUtil.loadLabels("assets/" + labelFileName);
-      await loadClasses();
-      for (int i = 0; i < _labels!.length; i++) {
-        if (!_filterClasses!.contains(_labels![i])) {
-          _labels![i] = "?";
-        }
-      }
     } catch (e) {
       print("Error while loading labels: $e");
     }
@@ -105,6 +91,9 @@ class Classifier {
     imageProcessor ??= ImageProcessorBuilder()
         .add(ResizeWithCropOrPadOp(padSize!, padSize!))
         .add(ResizeOp(inputSize, inputSize, ResizeMethod.BILINEAR))
+        // .add(NormalizeOp(127.5, 127.5))
+        // .add(DequantizeOp(128.0, 1 / 128.0))
+        // .add(QuantizeOp(0,1))
         .build();
     inputImage = imageProcessor!.process(inputImage);
     return inputImage;
@@ -125,34 +114,31 @@ class Classifier {
     TensorImage inputImage = TensorImage.fromImage(image);
 
     // Pre-process TensorImage
-    inputImage = getProcessedImage(inputImage);
+    try {
+      inputImage = getProcessedImage(inputImage);
+    } on Exception catch (e) {
+      print(e);
+    }
 
     var preProcessElapsedTime =
         DateTime.now().millisecondsSinceEpoch - preProcessStart;
     // TensorBuffers for output tensors
-    TensorBuffer outputLocations = TensorBufferFloat(_outputShapes[0]);
-    // TensorBuffer outputClassScores = TensorBufferFloat(_outputShapes[7]);
-    TensorBuffer outputClasses = TensorBufferFloat(_outputShapes[1]);
-    TensorBuffer outputScores = TensorBufferFloat(_outputShapes[2]);
-    TensorBuffer numLocations = TensorBufferFloat(_outputShapes[3]);
+    TensorBuffer outputScores = TensorBufferFloat(_outputShapes[0]);
 
-    // print(outputLocations.shape);
-    // print(outputClasses.shape);
     // print(outputScores.shape);
-    // print(numLocations.shape);
-    // print(_interpreter.getInputTensors());
-    // print(_interpreter.getOutputTensors());
-
+    // print(_interpreter!.getInputTensors());
+    // QuantizationParams inputParams = interpreter!.getInputTensor(0).params;
+    // print(inputParams.toString());
+    // print(_interpreter!.getOutputTensors());
+    // QuantizationParams outputParams = interpreter!.getOutputTensor(0).params;
+    // print(outputParams.toString());
     // Inputs object for runForMultipleInputs
     // Use [TensorImage.buffer] or [TensorBuffer.buffer] to pass by reference
     List<Object> inputs = [inputImage.buffer];
 
     // Outputs map
     Map<int, Object> outputs = {
-      0: outputLocations.buffer,
-      1: outputClasses.buffer,
-      2: outputScores.buffer,
-      3: numLocations.buffer,
+      0: outputScores.buffer,
     };
 
     var inferenceTimeStart = DateTime.now().millisecondsSinceEpoch;
@@ -163,56 +149,64 @@ class Classifier {
 
     // print("check1");
     // run inference
-    _interpreter!.runForMultipleInputs(inputs, outputs);
+    try {
+      _interpreter!.runForMultipleInputs(inputs, outputs);
+    } on Exception catch (e) {
+      print(e);
+    }
     // print("pass");
     var inferenceTimeElapsed =
         DateTime.now().millisecondsSinceEpoch - inferenceTimeStart;
 
-    // Maximum number of results to show
-    int resultsCount = min(numResults, numLocations.getIntValue(0));
-    // int resultsCount = NUM_RESULTS;
-    // int resultsCount = numLocations.getIntValue(0);
-    print(resultsCount);
-
-    // Using labelOffset = 1 as ??? at index 0
-    int labelOffset = -1;
-
     // Using bounding box utils for easy conversion of tensorbuffer to List<Rect>
-    List<Rect> locations = BoundingBoxUtils.convert(
-      tensor: outputLocations,
-      valueIndex: [1, 0, 3, 2],
-      boundingBoxAxis: 2,
-      boundingBoxType: BoundingBoxType.BOUNDARIES,
-      coordinateType: CoordinateType.RATIO,
-      height: inputSize,
-      width: inputSize,
-    );
+    // List<Rect> locations = BoundingBoxUtils.convert(
+    //   tensor: outputLocations,
+    //   valueIndex: [1, 0, 3, 2],
+    //   boundingBoxAxis: 2,
+    //   boundingBoxType: BoundingBoxType.BOUNDARIES,
+    //   coordinateType: CoordinateType.RATIO,
+    //   height: inputSize,
+    //   width: inputSize,
+    // );
     // print("converted bounding boxes");
     List<Recognition> recognitions = [];
+    final List<double> scoreList = outputScores.getDoubleList();
+    // for (var element in scoreList) {
+    //   print(element);
+    // }
 
-    for (int i = 0; i < resultsCount; i++) {
-      // Prediction score
-      var score = outputScores.getDoubleValue(i);
+    final int maxIndex = scoreList.indexOf(scoreList.reduce(max));
+    final double score = scoreList[maxIndex];
+    if (score > threshold) {
+      print(score);
+      // Using labelOffset = 1 as ??? at index 0
+      int labelOffset = 1;
+      // Label string
+      var labelIndex = maxIndex + labelOffset;
+      print(labelIndex);
+      final labels = [
+        'Awning', 'Basketball Court', 'Bicycle' ,'Bicycle Stand', 'Broad Walk' ,'Bush',
+        'Electric Scooter' ,'Garbage Bin' ,'Golf cart' ,'Gym Equipment', 'Lamp Post',
+        'Lifeguard', 'Rail', 'Recycling Area', 'Sign Board', 'Washroom Toilet (Male)',
+        'Washroom Toilet (Female)', 'beach chair' ,'bench' ,'boat' 'building',
+        'chair', 'flag with pole' ,'flower', 'grass', 'palm tree', 'play area',
+        'shower', 'stairs' ,'table', 'tree' ,'umbrella'
+      ];
+      // var label = _labels!.elementAt(labelIndex);
+      var label = labels.elementAt(labelIndex);
+      print(label);
+      if (label != "?") {
+        // inverse of rect
+        // [locations] corresponds to the image size 300 X 300
+        // inverseTransformRect transforms it our [inputImage]
+        Rect transformedRect = Rect.fromCenter(
+            center: const Offset(0, 0), width: 240, height: 240);
+        // Rect transformedRect = imageProcessor!
+        //     .inverseTransformRect([locations[i]], image.height, image.width);
 
-      if (score > threshold) {
-        print(score);
-        // Label string
-        var labelIndex = outputClasses.getIntValue(i) + labelOffset;
-        print(labelIndex);
-
-        var label = _labels!.elementAt(labelIndex);
-        print(label);
-        if (label != "?") {
-          // inverse of rect
-          // [locations] corresponds to the image size 300 X 300
-          // inverseTransformRect transforms it our [inputImage]
-          Rect transformedRect = imageProcessor!
-              .inverseTransformRect(locations[i], image.height, image.width);
-
-          recognitions.add(
-            Recognition(i, label, score, transformedRect),
-          );
-        }
+        recognitions.add(
+          Recognition(maxIndex, label, score, transformedRect),
+        );
       }
     }
 
